6a73c78dcac6f2e13907d6bac17d105d77202cd8
stak21 <stakashima21@gmail.com>
2018-11-12 23:07:47 +0000

    factored strtow

--- strtow.c
+++ strtow.c
@@ -15,14 +15,55 @@ char  **strtow(char *str)
 {
 	char **words;
 	int i, start, found;
-	int count;
+	unsigned int count;
 
-	count = 0;
 	start = i = 0;
-	found = 0;
+	count = word_count;	
+
+	words = malloc(sizeof(char *) * (count + 1));
+	if (!words)
+	{
+		free(words);
+		return (NULL);
+	}
+	get_word(words);
+
+
+	return (words);
+}
+
+/**
+* cpy_str - clips a word from the string
+* @end: the end of the word
+* @s: the string to clip the word from
+* @word_box: the array to store the word in
+*/
+void cpy_str(int end, char *s, char **word_box)
+{
+	int i;
+
+	(*word_box) = malloc(sizeof(char) * end + 1);
+	for (i = 0; i < end; i += 1)
+		(*word_box)[i] = s[i];
+	(*word_box)[i] = '\0';
+}
+
+/**
+* word_count - counts the number of words in a string
+* @str: a pointer to the string
+* Return: the number of words in a string
+*/
+
+unsigned int word_count(char *str)
+{
+	unsigned int count, i;
+	int found;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
+
+	found = i = count = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ' || str[i + 1] == '\0')
@@ -41,15 +82,18 @@ char  **strtow(char *str)
 			found = 1;
 		i += 1;
 	}
-	i = 0;
-	words = malloc(sizeof(char *) * (count + 1));
-	if (!words)
-	{
-		free(words);
-		return (NULL);
-	}
-	count = 0;
-	found = 0;
+	return (count);
+}
+/**
+* get_word - get a word from a string to copy
+* @str: a pointer to the string
+* Return: the array of words 
+*/
+void get_word(char **array)
+{
+	char *str = *array;	
+
+	count = i = start = found = 0;
 
 	while (str[i] != '\0')
 	{
@@ -81,24 +125,4 @@ char  **strtow(char *str)
 	}
 
 	words[count] = NULL;
-	return (words);
 }
-
-/**
-* cpy_str - clips a word from the string
-* @end: the end of the word
-* @s: the string to clip the word from
-* @word_box: the array to store the word in
-*/
-void cpy_str(int end, char *s, char **word_box)
-{
-	int i;
-
-	(*word_box) = malloc(sizeof(char) * end + 1);
-	if (!(*word_box))
-		return;
-	for (i = 0; i < end; i += 1)
-		(*word_box)[i] = s[i];
-	(*word_box)[i] = '\0';
-}
-
