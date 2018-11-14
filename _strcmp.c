0f8ab5d86f4fc0b5d4008a21820fd97b67fc1932
truong21 <543@holbertonschool.com>
2018-11-14 05:10:54 +0000

    remove condition

--- _strcmp.c
+++ _strcmp.c
@@ -16,11 +16,8 @@ int _strcmp(const char *s1, const char *s2)
 	
 	while (s1[i] == s2[i])
 		i += 1;
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{	
-		printf("why is this true\n");
+	if (s1[i] == '\0')
 		return (1);
-	}
 	else
 		return (-1);
 }
