aeaf7224453d283ce1df88b6100b578c7aa0240f
truong21 <543@holbertonschool.com>
2018-11-19 20:26:51 +0000

    updated header file

--- fuji.h
+++ fuji.h
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
+#include <signal.h>
 
 /**
  * struct linked_PATH - singly linked list of PATH directories
@@ -24,6 +25,22 @@ typedef struct linked_PATH
 	char *dir;
 	struct linked_PATH *next;
 } lp_t;
+/**
+ * struct builtins - builtin command and functions for shell
+ * @cmd: cmd name
+ * @f: pointer to builtin functions
+ *
+ * Description: struct for builtin functions
+ */
+typedef struct builtins
+{
+	char *cmd;
+	void (*f)(char **args);
+} built_t;
+/* Function Declarations for builtin shell commands */
+void fuji_env(char **args);
+void fuji_exit(char **args);
+void fuji_built(char **args);
 
 lp_t *linked_PATH(void);
 lp_t *add_node_end(lp_t **head, char *str);
@@ -32,6 +49,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_getenv(const char *name);
 int _strlen(char *s);
 int _strcmp(const char *s1, const char *s2);
+int _strcmp2(const char *s1, const char *s2);
 int _which(int argc, char *argv[]);
 char **strtow(char *str);
 void get_word(char **array, char *str);
@@ -40,4 +58,5 @@ void cpy_str(int end, char *s, char **word_box);
 char *str_concat(char *s1, char *s2);
 void check_path(char **string);
 void free_lp(char *str1, lp_t *head);
+void signal_handler(int signo);
 #endif
